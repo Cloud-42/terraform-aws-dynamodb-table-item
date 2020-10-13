904f2a6480182f6dd224cc6f8aac42f5d756cccd
Cloud-42 <hello@cloud42.io>
2020-10-13 10:04:09 +0000

    Optional range_key

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@
 resource "aws_dynamodb_table_item" "example" {
   table_name = var.table_name 
   hash_key   = var.hash_key
+  range_key  = var.range_key   
 
   item = var.item
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
 variable "table_name" {}
 variable "hash_key" {}
 variable "item" {}
+variable "range_key" {
+  default = null
+}
